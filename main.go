fda22ea34d76ab09dacdba599bd811e48a6b288d
cynen <cynen@foxmail.com>
2023-01-03 10:19:24 +0800

    添加 Readme描述

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
+	// 1.使用原生的 http 实现 web服务器.
 	//gindemo.Main()
+
+	// 2.自定义实现gin框架.
 	engine := gindemo.Default()
 	engine.Get("/hello", func(w http.ResponseWriter, request *http.Request) {
 		w.Write([]byte("hello!"))
